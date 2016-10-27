b2cbf729f6e58f10aba4823af0ef0080fc084bb2
Jason Arnold <jason.arnold.00@gmail.com>
2016-10-26 21:51:36 -0400

    first commit

---
+++ db/migrate/20161024165853_create_candidates.rb
@@ -0,0 +1,11 @@
+class CreateCandidates < ActiveRecord::Migration[5.0]
+  def change
+    create_table :candidates do |t|
+      t.string :name
+      t.string :party
+      t.integer :delegate_count
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20161024165947_create_states.rb
@@ -0,0 +1,12 @@
+class CreateStates < ActiveRecord::Migration[5.0]
+  def change
+    create_table :states do |t|
+      t.string :name
+      t.integer :delegates
+      t.string :predicted
+      t.boolean :called
+
+      t.timestamps
+    end
+  end
+end
